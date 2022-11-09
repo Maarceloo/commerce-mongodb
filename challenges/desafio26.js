004dcf1d81b59ac909b62a7ba5702cf7219e8c08
Marcelo De Lima <delima_marcelo@outlook.com>
2022-11-09 18:52:16 -0300

    feat: 24, 25 e 26

---
+++ challenges/desafio26.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        $and: [{ percentual: { $gt: 20 } }, { percentual: { $lt: 40 } }],
+      },
+    },
+  },
+  { $push: { tags: "contém sódio" } },
+);
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
